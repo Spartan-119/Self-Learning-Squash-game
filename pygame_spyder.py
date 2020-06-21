e482eb8ea92f43ef0af63d8b458835e2fc70de2f
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-06-21 08:17:43 +0530

    Update pygame_spyder.py

--- pygame_spyder.py
+++ pygame_spyder.py
@@ -11,6 +11,35 @@ import pygame as pg
 WIDTH = 1200
 HEIGHT = 600
 BORDER = 20
+VELOCITY = -1
+
+# defining the classes
+
+# Ball class
+class Ball:
+    RADIUS = 20
+    
+    def __init__(self, x, y, vx, vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        
+    def show(self, color):
+        global screen
+        pg.draw.circle(screen, color, (self.x, self.y), self.RADIUS)
+        
+    # function to move the ball
+    def update(self, color):
+        self.x = self.x + self.vx
+        self.y = self.y + self.vy
+
+# Paddle class
+class Paddle:
+    pass
+
+# creating objects
+ballPlay = Ball(WIDTH - Ball.RADIUS, HEIGHT//2, VELOCITY, 0)
 
 pg.init()
 
@@ -24,12 +53,15 @@ pg.draw.rect(screen, fgColor, pg.Rect((0, 0), (BORDER, HEIGHT)))
 pg.draw.rect(screen, fgColor, pg.Rect((0, HEIGHT - BORDER, WIDTH, BORDER)))
 
 # to display the console screen
-pg.display.flip()
+ballPlay.show(fgColor) # to show the ball
 
 # to close the console
 while True:
     e = pg.event.poll()
     if e.type == pg.QUIT:
         break
+    
+    pg.display.flip()
+    ballPlay.update(fgColor)
 
 pg.quit()
