579775f5dece262692a8fd5f998d5940ffef46f9
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-09-12 22:39:44 +0530

    Add files via upload

--- pygame_spyder.py
+++ pygame_spyder.py
@@ -1,100 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
+# import statements
 import pygame as pg
 
-# declaring the variables
-WIDTH = 1200
+pg.init()
+
+# declaring global variables
 HEIGHT = 600
+WIDTH = 1200
 BORDER = 20
-VELOCITY_X = 1
-VELOCITY_Y = 1
+bgcolor = "Black"
+ball_color = "Yellow"
 
-# defining the classes
 
-# Ball class
+
 class Ball:
-    RADIUS = 20
+    
+    RADIUS = 15
     
     def __init__(self, x, y, vx, vy):
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
-        
+    
+    # method to show the Ball
     def show(self, color):
-        global screen
         pg.draw.circle(screen, color, (self.x, self.y), self.RADIUS)
-        
-    # function to move the ball
-    def update(self):
-        global bgColor, fgColor
-        
-        new_x = self.x + self.vx
-        new_y = self.y + self.vy
-        
-        # conditions to manage the collision of the ball with the border
-        if new_x < BORDER + self.RADIUS:
-            self.vx = -self.vx
-        
-        elif new_y < BORDER + self.RADIUS or new_y > HEIGHT - BORDER - self.RADIUS:
-            self.vy = -self.vy
-        
-        else:
-            self.show(bgColor)
-            self.x = self.x + self.vx
-            self.y = self.y + self.vy
-            self.show(fgColor)
-
-# Paddle class
-class Paddle:
-    WIDTH = 20
-    HEIGHT = 100
     
-    def __init__(self, y):
-        self.y = y
+    # method to update the position of the Ball
+    # def update(self, )
     
-    def show(self, color):
-        global screen
-        pg.draw.rect(screen, color, pg.Rect(WIDTH - self.WIDTH, self.y - self.HEIGHT//2))
-    
-    def update(self):
-        self.show(pg.Color('Black'))
-        self.y = pg.mouse.get_pos()[1]
-        self.show(pg.Color('Yellow'))
-
-# creating objects
-ballPlay = Ball(WIDTH - Ball.RADIUS, HEIGHT//2, -VELOCITY_X, -VELOCITY_Y)
-paddle = Paddle(HEIGHT//2)
-
 pg.init()
 
+# displaying the screen
 screen = pg.display.set_mode((WIDTH, HEIGHT))
+fgcolor = pg.Color("White")
 
-bgColor = pg.Color('Black')
-fgColor = pg.Color('Yellow')
+# now we will draw the screen
+pg.draw.rect(screen, fgcolor, pg.Rect((0, 0), (WIDTH, BORDER)))
+pg.draw.rect(screen, fgcolor, pg.Rect(0, 0, BORDER, HEIGHT))
+pg.draw.rect(screen, fgcolor, pg.Rect(0, HEIGHT - BORDER, WIDTH, BORDER))
 
-# drawing the borders
-pg.draw.rect(screen, fgColor, pg.Rect((0, 0), (WIDTH, BORDER)))
-pg.draw.rect(screen, fgColor, pg.Rect((0, 0), (BORDER, HEIGHT)))
-pg.draw.rect(screen, fgColor, pg.Rect((0, HEIGHT - BORDER, WIDTH, BORDER)))
+pg.display.flip()
 
-# to display the console screen
-ballPlay.show(fgColor) # to show the ball
-paddle.show(fgColor)
-
-# to close the console
 while True:
     e = pg.event.poll()
     if e.type == pg.QUIT:
         break
-    
-    pg.display.flip()
-    ballPlay.update()
-    paddle.update()
 
 pg.quit()
