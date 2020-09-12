c8d488b41cf5bf1933174bebef324e9daab92680
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-09-13 01:37:01 +0530

    Add files via upload

--- pygame_spyder.py
+++ pygame_spyder.py
@@ -8,9 +8,16 @@ HEIGHT = 600
 WIDTH = 1200
 BORDER = 20
 bgcolor = "Black"
-ball_color = "Yellow"
+fgcolor = pg.Color("White")
+ball_color = pg.Color("Yellow")
+paddle_color = pg.Color("Green")
+VELOCITY = 5
+FRAMERATE = 60
 
+pg.init()
 
+# displaying the screen
+screen = pg.display.set_mode((WIDTH, HEIGHT))
 
 class Ball:
     
@@ -27,13 +34,33 @@ class Ball:
         pg.draw.circle(screen, color, (self.x, self.y), self.RADIUS)
     
     # method to update the position of the Ball
-    # def update(self, )
-    
-pg.init()
+    def update(self, ):
+        pass
 
-# displaying the screen
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-fgcolor = pg.Color("White")
+# ------- PADDLE --------
+class Paddle:
+    
+    WIDTH = 20
+    HEIGHT = 100
+    
+    def __init__(self, y):
+        self.y = y # because we will only move the paddle along Y axis
+    
+    # method to show the paddle
+    def show(self, color, x):
+        pg.draw.rect(screen, color, pg.Rect(x, self.y - self.HEIGHT//2,self.WIDTH,self.HEIGHT))
+    
+    # method to update the paddle
+    def update(self, ):
+        pass
+
+# ------ MAKING OBJECTS -------
+ball = Ball(WIDTH - Ball.RADIUS - Paddle.WIDTH, HEIGHT//2, -VELOCITY, -VELOCITY)
+ball.show(ball_color)
+
+paddle = Paddle(HEIGHT//2)
+paddle.show(paddle_color, WIDTH - Paddle.WIDTH)
+
 
 # now we will draw the screen
 pg.draw.rect(screen, fgcolor, pg.Rect((0, 0), (WIDTH, BORDER)))
