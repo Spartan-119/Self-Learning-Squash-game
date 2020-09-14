7df71764b3a89f010fc76492eec72c6dcda8f048
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-09-14 19:14:32 +0530

    Add files via upload

--- pygame_spyder.py
+++ pygame_spyder.py
@@ -7,14 +7,14 @@ pg.init()
 HEIGHT = 600
 WIDTH = 1200
 BORDER = 20
-bgcolor = "Black"
+bgcolor = pg.Color("black")
 fgcolor = pg.Color("White")
 ball_color = pg.Color("Yellow")
 paddle_color = pg.Color("Green")
 VELOCITY = 5
 FRAMERATE = 60
+clock = pg.time.Clock()
 
-pg.init()
 
 # displaying the screen
 screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -33,9 +33,33 @@ class Ball:
     def show(self, color):
         pg.draw.circle(screen, color, (self.x, self.y), self.RADIUS)
     
+    ''' To move the ball around, we must UPDATE the position of the ball
+        asper the command given by an input device 
+        (in this case it is Mouse cursor)'''
     # method to update the position of the Ball
-    def update(self, ):
-        pass
+    def update(self): #, paddle_y, paddle_WIDTH, paddle_HEIGHT):
+        newx = self.x + self.vx
+        newy = self.y + self.vy
+        
+        # logic to bounce back the ball off the walls
+        
+        ''' for along the X axis '''
+        '''if paddle_y - paddle_HEIGHT//2 <= newy + self.RADIUS and newy - self.RADIUS <= paddle_y + paddle_HEIGHT//2 \
+            and (newx + self.RADIUS >= WIDTH - paddle_WIDTH or newx - self.RADIUS <= Paddle.WIDTH):
+            self.vx = - self.vx'''
+        
+        if newx < BORDER + self.RADIUS: #or newx > HEIGHT - BORDER - self.RADIUS:
+            self.vx = - self.vx
+        
+        ''' for along the Y axis '''
+        if newy < BORDER + self.RADIUS or newy > HEIGHT - BORDER - self.RADIUS:
+            self.vy = - self.vy
+                    
+        # to show the moving ball
+        self.show(bgcolor)
+        self.x += self.vx
+        self.y += self.vy
+        self.show(ball_color)
 
 # ------- PADDLE --------
 class Paddle:
@@ -50,9 +74,19 @@ class Paddle:
     def show(self, color, x):
         pg.draw.rect(screen, color, pg.Rect(x, self.y - self.HEIGHT//2,self.WIDTH,self.HEIGHT))
     
-    # method to update the paddle
-    def update(self, ):
-        pass
+    ''' To move the paddle, we must UPDATE the position of the paddle 
+        it asper the command given by an input device 
+        (in this case it is Mouse cursor)'''
+    # method to Move the paddle
+    def update(self,mouse,x):
+        if not(mouse - self.HEIGHT//2 <= BORDER or mouse + self.HEIGHT//2 >= HEIGHT - BORDER):
+            self.show(bgcolor,x)
+            self.y = mouse
+            self.show(paddle_color,x)
+        elif mouse + self.HEIGHT//2 <= BORDER:
+            self.y = self.HEIGHT//2 + BORDER
+        else:
+            self.y = HEIGHT -self.HEIGHT//2 - BORDER
 
 # ------ MAKING OBJECTS -------
 ball = Ball(WIDTH - Ball.RADIUS - Paddle.WIDTH, HEIGHT//2, -VELOCITY, -VELOCITY)
@@ -67,11 +101,35 @@ pg.draw.rect(screen, fgcolor, pg.Rect((0, 0), (WIDTH, BORDER)))
 pg.draw.rect(screen, fgcolor, pg.Rect(0, 0, BORDER, HEIGHT))
 pg.draw.rect(screen, fgcolor, pg.Rect(0, HEIGHT - BORDER, WIDTH, BORDER))
 
-pg.display.flip()
+#pg.display.flip()
 
 while True:
     e = pg.event.poll()
     if e.type == pg.QUIT:
         break
+    
+    ball.update() #paddle.y, paddle.WIDTH, paddle.HEIGHT)
+    paddle.update(pg.mouse.get_pos()[1],0)
+    
+    ''' This method should be called once per frame. It will compute how many 
+        milliseconds have passed since the previous call.
+        If you pass the optional framerate argument the function 
+        will delay to keep the game running slower 
+        than the given ticks per second. 
+        This can be used to help limit the runtime speed of a game. 
+        By calling Clock.tick(40) once per frame, the program will 
+        never run at more than 40 frames per second. '''
+        
+    '''
+    FPS, Frames Per Second, is the number of frames shown per unit of time.
+    1 / FPS is the amount of time should pass between each frame.
+    Tick is just a measure of time in PyGame.
+
+    clock.tick(60) means that for every second at most 60 frames should pass.
+    '''    
+    clock.tick(FRAMERATE)
+    
+    # to update the contents of the ENTIRE screen
+    pg.display.flip()
 
 pg.quit()
